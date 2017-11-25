b6a757c00c7abba1a3dd0d92a9712bd3cdfe3a4e
AlexBHdez <abhbretones@gmail.com>
2017-11-25 13:18:24 +0100

    add drawboard functionality

---
+++ game.js
@@ -0,0 +1,20 @@
+function Game (options) {
+  this.snake = undefined;
+  this.food = undefined;
+  this.rows = options.rows;
+  this.columns = options.columns;
+  this.ctx = options.ctx;
+}
+
+Game.prototype._drawBoard = function () {
+  for (var columnIndex = 0; columnIndex < this.columns; columnIndex ++) {
+    for (var rowIndex = 0; rowIndex < this.rows; rowIndex ++) {
+      this.ctx.fillStyle = '#E3D4AB';
+      this.ctx.fillRect(columnIndex * 10, rowIndex * 10, 10, 10);
+    }
+  }
+};
+
+Game.prototype.start = function () {
+  this._drawBoard();
+};
---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var game;
+  var canvas = document.getElementById('snake');
+  var ctx = canvas.getContext('2d');
+
+  game = new Game({
+    rows: canvas.width / 10,
+    columns: canvas.height / 10,
+    snake: undefined,
+    ctx: ctx,
+  });
+
+  game.start();
+
+});
